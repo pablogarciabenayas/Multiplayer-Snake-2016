b2325d98e08c26722756ab55d87bbd183e76a9e9
pablogarciabenayas <pablogarciabenayas@gmail.com>
2016-04-11 21:50:30 +0200

    Inicialización del repositorio

---
+++ mp-snake-2016/src/mp/snake/pkg2016/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp.snake.pkg2016;
+
+import javax.sound.midi.SysexMessage;
+
+/**
+ *
+ * @author pablo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola repositorio!");
+    }
+    
+}
